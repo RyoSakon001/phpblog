81f09d4cb96997921e5ae3351472ca3236951a40
sakon-ziwave <81784300+sakon-ziwave@users.noreply.github.com>
2022-04-13 23:39:19 +0900

    Delete db_connect.php

--- db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-function db_connect(){
-    $username = 'ryo';
-    $password = 'y5ad8mjN';
-    $pdo = new PDO("mysql:dbname=phpblog;host=localhost", $username, $password);
-    return $pdo;
-}
-?>
