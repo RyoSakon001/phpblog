a81d8b093cd2e150000fc79bc55f5cb0621a57af
sakon-ziwave <ryo.development0320@gmail.com>
2022-04-13 23:46:30 +0900

    全体的なバグの改修

--- db_connect.template.php
+++ db_connect.template.php
@@ -2,7 +2,8 @@
 function db_connect(){
     $username = 'username';
     $password = 'password';
-    $pdo = new PDO("mysql:dbname=DBNAME;host=localhost", $username, $password);
+    $db_name = 'db_name';
+    $pdo = new PDO("mysql:dbname=".$db_name.";host=localhost", $username, $password);
     return $pdo;
 }
 ?>
