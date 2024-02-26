116dbc301c493979df381536bb5e7d1802a2887a
RyoSakon001 <ryo.development0320@gmail.com>
2024-02-26 14:28:35 +0900

    namespaceについて理解

--- practice/call.php
+++ practice/call.php
@@ -1,13 +1,28 @@
 <?php
 
-namespace japan;
+// use asia\japan\kanto\tokyo as area;
+// use asia\japan\kanto\tokyo;
+// use asia\japan\kanto\tokyo\{flower, bird, park};
+// use asia\japan\kanto\{kanagawa\flower, chiba\bird, tochigi\park};
+// use function asia\japan\kanto\tokyo\getPref;
+// use function asia\japan\kanto\tokyo\hogeFunc;
 
+namespace sakon;
+
+echo __NAMESPACE__;
+die();
+
+require_once 'tokyo.php';
 require_once 'sato.php';
 require_once 'suzuki.php';
 // require_once 'root.php';
 require_once 'root2.php';
 
 // echo japan\tokyo\chiyoda\sato\sayHello();
-echo satooo\sayHello();
-echo \suzuki\sayHello();
-echo sayHello();
+// echo satooo\sayHello();
+// echo \suzuki\sayHello();
+// echo sayHello();
+
+// echo area\pref::getPref();
+// echo tokyo\pref::getPref();
+// echo hogeFunc();
---
+++ practice/tokyo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace asia\japan\kanto\tokyo;
+
+
+function hogeFunc()
+{
+  echo 'hogehoge';
+}
+
+class pref
+{
+  public static function getPref()
+  {
+    return 'tokyo';
+  }
+}
